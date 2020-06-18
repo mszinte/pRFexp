6fb20aeacb08a10f42d5d5b3cd2d4d9ab23fc668
m.szinte <martin.szinte@gmail.com>
2020-06-18 10:55:38 +0200

    changes in testing room

--- main/expLauncher.m
+++ main/expLauncher.m
@@ -25,16 +25,16 @@ Screen('CloseAll');clear all;clear mex;clear functions;close all;home;AssertOpen
 % General settings
 % ----------------
 const.expName           =   'pRFexp';       % experiment name
-const.expStart          =   0;              % Start of a recording exp                          0 = NO  , 1 = YES
+const.expStart          =   1;              % Start of a recording exp                          0 = NO  , 1 = YES
 const.checkTrial        =   0;              % Print trial conditions (for debugging)            0 = NO  , 1 = YES
-const.writeLogTxt       =   1;              % write a log file in addition to eyelink file      0 = NO  , 1 = YES
+const.writeLogTxt       =   0;              % write a log file in addition to eyelink file      0 = NO  , 1 = YES
 const.genStimuli        =   0;              % Generate all stimuli                              0 = NO  , 1 = YES
 const.drawStimuli       =   0;              % Draw stimuli generated                            0 = NO  , 1 = YES
 const.mkVideo           =   0;              % Make a video of a run                             0 = NO  , 1 = YES
 
 % External controls
 % -----------------
-const.tracker           =   0;              % run with eye tracker                              0 = NO  , 1 = YES
+const.tracker           =   1;              % run with eye tracker                              0 = NO  , 1 = YES
 const.scanner           =   0;              % run in MRI scanner                                0 = NO  , 1 = YES
 const.scannerTest       =   1;              % run with T returned at TR time                    0 = NO  , 1 = YES
 const.room              =   2;              % run in MRI or eye-tracking room                   1 = MRI , 2 = eye-tracking
@@ -46,8 +46,8 @@ const.cond_run_num      =   [01;02;03;04;05];
 
 % Desired screen setting
 % ----------------------
-const.desiredFD         =   60;            % Desired refresh rate
-fprintf(1,'\n\n\tDon''t forget to change before testing\n');
+const.desiredFD         =   120;            % Desired refresh rate
+%fprintf(1,'\n\n\tDon''t forget to change before testing\n');
 const.desiredRes        =   [1920,1080];    % Desired resolution
 
 % Path
