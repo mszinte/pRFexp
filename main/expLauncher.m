ad58a9bf5d8af7d28e68acc95f915ea2426b4375
moritavan <vvanessamorita@gmail.com>
2020-09-14 13:12:01 +0200

    retur++

--- main/expLauncher.m
+++ main/expLauncher.m
@@ -28,7 +28,7 @@ const.expName           =   'pRFexp';       % experiment name
 const.expStart          =   1;              % Start of a recording exp                          0 = NO  , 1 = YES
 const.checkTrial        =   0;              % Print trial conditions (for debugging)            0 = NO  , 1 = YES
 const.writeLogTxt       =   0;              % write a log file in addition to eyelink file      0 = NO  , 1 = YES
-const.genStimuli        =   1;              % Generate the stimuli                              0 = NO  , 1 = YES
+const.genStimuli        =   0;              % Generate the stimuli                              0 = NO  , 1 = YES
 const.drawStimuli       =   0;              % Draw stimuli generated                            0 = NO  , 1 = YES
 const.mkVideo           =   0;              % Make a video of a run                             0 = NO  , 1 = YES
 
