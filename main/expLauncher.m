4d97113603f8992ac9bf8af5b0c4b8d8dcba31d1
Martin Szinte <martin.szinte@gmail.com>
2024-04-11 11:12:17 +0200

    sub-01 training

--- main/expLauncher.m
+++ main/expLauncher.m
@@ -28,10 +28,11 @@ const.mkVideo = 0;              % Make a video of a run
 
 % External controls
 const.tracker = 1;              % run with eye tracker (0 = NO, 1 = YES)
-const.comp = 1;                 % run in which computer (1 = MRI; 2 = Can laptop; 3 = Diplay++)
-const.scanner = 1;              % run in MRI scanner (0 = NO, 1 = YES)
+const.comp = 3;
+% run in which computer (1 = MRI; 2 = Can laptop; 3 = Diplay++)
+const.scanner = 0;              % run in MRI scanner (0 = NO, 1 = YES)
 const.scannerTest = 0;          % fake scanner trigger (0 = NO, 1 = YES)
-const.training = 0;             % training session (0 = NO, 1 = YES)
+const.training = 1;             % training session (0 = NO, 1 = YES)
 const.run_total = 5;            % number of run in total
 
 % Desired screen setting
