c9b323e7f50b0d05a306da425b09c8f1b92ee027
Martin Szinte <martin.szinte@gmail.com>
2024-07-03 16:18:58 +0200

    settings

--- main/expLauncher.m
+++ main/expLauncher.m
@@ -23,17 +23,17 @@ const.expName = 'pRF';          % experiment name
 const.expStart = 1;             % Start of a recording exp (0 = NO, 1 = YES)
 const.checkTrial = 0;           % Print trial conditions (0 = NO, 1 = YES)
 const.genStimuli = 0;           % Generate the stimuli (0 = NO, 1 = YES)
-const.drawStimuli = 0;          % Draw stimuli generated (0 = NO, 1 = YES)
+const.drawStimuli = 1;          % Draw stimuli generated (0 = NO, 1 = YES)
 const.mkVideo = 0;              % Make a video of a run
 
 % External controls
-const.tracker = 1;              % run with eye tracker (0 = NO, 1 = YES)
+const.tracker = 0;              % run with eye tracker (0 = NO, 1 = YES)
 const.comp = 3;
 % run in which computer (1 = MRI; 2 = Can laptop; 3 = Diplay++)
 const.scanner = 0;              % run in MRI scanner (0 = NO, 1 = YES)
 const.scannerTest = 0;          % fake scanner trigger (0 = NO, 1 = YES)
 const.training = 1;             % training session (0 = NO, 1 = YES)
-const.run_total = 5;            % number of run in total
+const.run_total = 1;            % number of run in total
 
 % Desired screen setting
 const.desiredFD = 120;          % Desired refresh rate
